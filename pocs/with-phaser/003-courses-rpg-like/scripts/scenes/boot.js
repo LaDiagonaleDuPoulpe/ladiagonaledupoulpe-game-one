b5faa4c5487f559cd8c95ef9bd59a493b2c74135
evan-boissonnot <commercial@boissonnot.Fr>
2019-07-17 09:16:49 +0200

    chaging level is ok, just for 2 levels ...

--- pocs/with-phaser/003-courses-rpg-like/scripts/scenes/boot.js
+++ pocs/with-phaser/003-courses-rpg-like/scripts/scenes/boot.js
@@ -7,14 +7,18 @@ export default class BootScene extends Phaser.Scene {
   }
 
   preload () {
+    
+
     // load in the tilemap
     this.load.tilemapTiledJSON('level1', 'assets/tilemaps/level1.json');
+    this.load.tilemapTiledJSON('level2', 'assets/tilemaps/level2.json');
     // load in the spritesheet
     this.load.spritesheet('RPGpack_sheet', 'assets/images/RPGpack_sheet.png', { frameWidth: 64, frameHeight: 64 });
     this.load.spritesheet('characters', 'assets/images/roguelikeChar_transparent.png', { frameWidth: 17, frameHeight: 17 });
+    this.load.image('portal', 'assets/images/raft.png');
   }
 
   create () {
-    this.scene.start('Game');
+    this.scene.start('Game', { level: 1, newGame: true, levels: [] });
   }
 };
--- pocs/with-phaser/003-courses-rpg-like/scripts/scenes/game.js
+++ pocs/with-phaser/003-courses-rpg-like/scripts/scenes/game.js
@@ -1,5 +1,6 @@
 import 'phaser';
 import Player from '../sprites/player';
+import Portal from '../sprites/portal';
 
 export default class GameScene extends Phaser.Scene {
     constructor(key) {
@@ -7,41 +8,65 @@ export default class GameScene extends Phaser.Scene {
         console.log('constructor');
     }
     
-    preload() {
+    init(data) {
+        this.data = data;
+        this.loadingLevel = false;
     }
     
     create() {
         this.scale.on('resize', this.resize, this);
         this.cursors = this.input.keyboard.createCursorKeys();
-
+        
         this.createMap();
         this.createPlayer();
-
+        
+        this.createPortal();
+        
         this.addCollisions();
+        
+        this.cameras.main.startFollow(this.player);
+        
     }
     
     update() {
         this.player.update(this.cursors);
     }
-
+    
+    createPortal() {
+        this.map.findObject('Portal', (obj) => {
+            if(this.data.level === 1) {
+                this.portal = new Portal(this, obj.x, obj.y - 68);
+            } else if (this.data.level == 2) {
+                this.portal = new Portal(this, obj.x, obj.y);
+            }
+        });
+    }
+    
     addCollisions() {
         this.physics.add.collider(this.player, this.blockedLayer);
+        this.physics.add.overlap(this.player, this.portal, this.loadNextLevel.bind(this));
     }
     
     createPlayer() {
-        console.log('create ?');
         this.map.findObject('Player', (obj) => {
-            console.log('create ?!!!!');
-            if (obj.type === 'StartingPosition') {
+            if(this.data.newGame && this.data.level ===1) {
+                if (obj.type === 'StartingPositionPortal') {
+                    this.player = new Player(this, obj.x, obj.y);
+                }
+            }
+            else {
                 this.player = new Player(this, obj.x, obj.y);
-                this.cameras.main.startFollow(this.player);
             }
         });
     }
     
     createMap() {
+        this.add.tileSprite(0, 0, 8000, 8000, 'RPGpack_sheet', 31);
+        
+        console.log(this.data.level);
+        
         // create the tilemap
-        this.map = this.make.tilemap({ key: 'level1' });
+        this.map = this.make.tilemap({ key: 'level' + this.data.level });
         // add tileset image
         this.tiles = this.map.addTilesetImage('RPGpack_sheet');
         // create our layers
@@ -55,6 +80,27 @@ export default class GameScene extends Phaser.Scene {
         let width = gameSize.width;
         let height = gameSize.height;
         
+        if (width === undefined) {
+            width = this.sys.game.config.width;
+        }
+        if (height === undefined) {
+            height = this.sys.game.config.height;
+        }
+        
         this.cameras.resize(width, height);
+    }
+    
+    loadNextLevel() {
+        if (! this.loadingLevel) {
+            this.cameras.main.fade(500, 0, 0, 0);
+            this.cameras.main.on('camerafadeoutcomplete', () => {
+              if (this.data.level === 1) {
+                this.scene.restart({ level: 2, newGame: false });
+              } else if (this.data.level === 2) {
+                this.scene.restart({ level: 1, newGame: false });
+              }
+            });
+            this.loadingLevel = true;
+          }
     }
 }
