8a9898258c5356f6e05c4346cd9d12d47c8f0d2d
evan-boissonnot <commercial@boissonnot.Fr>
2019-08-09 17:48:14 +0200

    preparing unit prefab

---
+++ pocs/with-phaser/004-courses-rpg-like-ff/src/prefabs/battle/unit.js
@@ -0,0 +1,57 @@
+import Prefab from '../prefab';
+import TitleScene from '../../scenes/title-scene';
+
+class Unit extends Prefab {
+    
+    constructor(scene, name, position, properties) {
+        super(scene, name, position, properties);        
+    }
+    
+    //#region public methods    
+    //#endregion
+    
+    //#region protected methods
+    initialize(scene, name, position, properties) {
+        super.initialize(scene, name, position, properties);
+        
+        const startingAnimationKey = createAnimation('idle');
+        createAnimation('attack1');
+        createAnimation('attack2');
+        createAnimation('hit');
+
+        this.anims.play(startingAnimationKey);
+
+        this.stats = properties.stats;
+    }   
+    //#endregion
+    
+    //#region internal methods
+    /**
+     * Creates an animation and return the animationKey
+     * @param {string} name 
+     * @param {string} animationName
+     * @returns Returns animation key 
+     */
+    createAnimation(name, animationName) {
+        const animationKey = name + '_' + animationName;
+
+        if (this.scene.anims.anims.has(animationKey)) {
+            const frameConfig = {
+                frames: properties.animations[animationName].frames,
+            };
+            const frames = this.scene.anims.generateFrameNumbers(this.texture.key, frameConfig);
+            
+            this.scene.anims.create({
+                key: animationKey,
+                frames: frames,
+                frameRate: properties.animations[animationName].fps,
+                repeat: -1
+            });
+        }
+
+        return animationKey;
+    }
+    //#endregion
+}
+
+export default Unit;
