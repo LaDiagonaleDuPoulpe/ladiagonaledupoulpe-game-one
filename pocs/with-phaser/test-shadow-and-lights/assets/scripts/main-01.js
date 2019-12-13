b41463d98d9c24a3577ec0d6dd45bb200dbfb299
evan-boissonnot <commercial@boissonnot.Fr>
2019-12-13 10:07:30 +0100

    changing name => spark

---
+++ pocs/with-phaser/test-shadow-and-lights/assets/scripts/main-01.js
@@ -0,0 +1,27 @@
+
+var config = {
+    type: Phaser.WEBGL,
+    width: 1960,
+    height: 1080,
+    backgroundColor: '#ababab',
+    parent: 'la-diagonale-du-poulpe',
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+
+}
+
+function create() {
+
+}
+
+function update() {
+    
+}
