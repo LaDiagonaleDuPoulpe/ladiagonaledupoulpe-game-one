55507143d37c225490f86d71ae442e5e5b633342
evan-boissonnot <commercial@boissonnot.Fr>
2019-12-23 07:55:24 +0100

    working with all directions

--- pocs/with-phaser/test-shadow-and-lights/assets/scripts/main-02.js
+++ pocs/with-phaser/test-shadow-and-lights/assets/scripts/main-02.js
@@ -27,12 +27,16 @@ let texture = undefined;
 console.log('game.state :', game.state);
 
 function preload() {
-    this.load.setPath('assets/images/Octo2 PNGs/octo3 down all');
-    this.load.multiatlas('character_sheet_down', 'octo3-down-all.json');
+    this.load.setPath('assets/images/Octo2 PNGs');
+    this.load.multiatlas('character_sheet_down', 'octo3 down all/octo3-down-all.json');
+    this.load.multiatlas('character_sheet_top', 'octo3 up all/octo3-top-all.json');
+    this.load.multiatlas('character_sheet_left', 'octo3 left all/octo3-left-all.json');
+    this.load.multiatlas('character_sheet_right', 'octo3 right all/octo3-right-all.json');
 }
 
 var light1 = null;
 var light2 = null;
+var capguy_anim = null;
 
 var character = null;
 function create() {
@@ -41,27 +45,55 @@ function create() {
 
     //character = this.add.sprite(500, 500, 'character');
 
-    var capguy_anim = this.add.sprite(500, 500, 'character_sheet_down');
+    capguy_anim = this.physics.add.sprite(500, 500, 'character_sheet_down');
     capguy_anim.setPipeline('Light2D');
 
+    this.physics.world.enable(capguy_anim);
 
     //var frameNames = this.anims.generateFrameNames('character_sheet', { start: 1, end: 5 });
     var frameNames = this.anims.generateFrameNames('character_sheet_down', { zeroPad: 3, prefix: 'octo3 down all_', suffix: '.png', start: 0, end: 15 });
-    this.anims.create({ key: 'idle', frames: frameNames, frameRate: 15, repeat: -1 });
-    capguy_anim.anims.play('idle');
+    this.anims.create({ key: 'idle-down', frames: frameNames, frameRate: 15, repeat: -1 });
+    //capguy_anim.anims.play('idle');
+
+    frameNames = this.anims.generateFrameNames('character_sheet_down', { zeroPad: 3, prefix: 'octo3 down all_', suffix: '.png', start: 45, end: 65 });
+    this.anims.create({ key: 'walk', frames: frameNames, frameRate: 25, repeat: -1 });
     
+
+    frameNames = this.anims.generateFrameNames('character_sheet_left', { zeroPad: 3, prefix: 'octo3 left all_', suffix: '.png', start: 0, end: 15 });
+    this.anims.create({ key: 'idle-left', frames: frameNames, frameRate: 15, repeat: -1 });
+    //capguy_anim.anims.play('idle');
+
+    frameNames = this.anims.generateFrameNames('character_sheet_left', { zeroPad: 3, prefix: 'octo3 left all_', suffix: '.png', start: 45, end: 69 });
+    this.anims.create({ key: 'walk-left', frames: frameNames, frameRate: 25, repeat: -1 });
+
+    frameNames = this.anims.generateFrameNames('character_sheet_right', { zeroPad: 3, prefix: 'octo3 right all_', suffix: '.png', start: 0, end: 15 });
+    this.anims.create({ key: 'idle-right', frames: frameNames, frameRate: 15, repeat: -1 });
+    //capguy_anim.anims.play('idle');
+
+    frameNames = this.anims.generateFrameNames('character_sheet_right', { zeroPad: 3, prefix: 'octo3 right all_', suffix: '.png', start: 45, end: 69 });
+    this.anims.create({ key: 'walk-right', frames: frameNames, frameRate: 25, repeat: -1 });
+
+    frameNames = this.anims.generateFrameNames('character_sheet_top', { zeroPad: 3, prefix: 'octo3 up all_', suffix: '.png', start: 0, end: 15 });
+    this.anims.create({ key: 'idle-top', frames: frameNames, frameRate: 15, repeat: -1 });
+    //capguy_anim.anims.play('idle');
+
+    frameNames = this.anims.generateFrameNames('character_sheet_top', { zeroPad: 3, prefix: 'octo3 up all_', suffix: '.png', start: 45, end: 69 });
+    this.anims.create({ key: 'walk-top', frames: frameNames, frameRate: 25, repeat: -1 });
+    
+    capguy_anim.anims.play('walk-top');
+
     //this.cameras.main.fadeIn(6000);
 
-    light1 = this.lights.addLight(1300, 300, 500).setColor(0xffffff).setIntensity(2);
-    var ellipse1 = new Phaser.Geom.Ellipse(light1.x, light1.y, 70, 100);
+    light1 = this.lights.addLight(100, 300, 500).setColor(0xffffff).setIntensity(2);
+    var ellipse1 = new Phaser.Geom.Ellipse(light1.x, light1.y, 700, 1000);
 
     light2 = this.lights.addLight(10, 600, 500).setColor(0xffffff).setIntensity(2);
-    var ellipse2 = new Phaser.Geom.Ellipse(light2.x, light2.y, 70, 1200);
+    var ellipse2 = new Phaser.Geom.Ellipse(light2.x, light2.y, 700, 1200);
 
-    capguy_anim.setPipeline('Light2D');
+    //capguy_anim.setPipeline('Light2D');
 
     this.time.addEvent({
-        delay: 50,
+        delay: 500,
         callback: function ()
         {
             Phaser.Geom.Ellipse.Random(ellipse1, light1);
@@ -91,6 +123,18 @@ function create() {
 
     this.lights.enable().setAmbientColor(0x333333);
 
+    // this.input.keyboard.on('keydown_W', function (event) {
+    //     capguy_anim.setVelocityY(15);
+    //     capguy_anim.anims.play('walk-down');
+    // });
+
+    // this.input.keyboard.on('keydown_Z', function (event) {
+    //     capguy_anim.setVelocityY(-15);
+    //     capguy_anim.anims.play('walk-top');
+    // });
+
+
+    this.cameras.main.startFollow(capguy_anim);
 }
 
 function update() {
