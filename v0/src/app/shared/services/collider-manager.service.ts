ec9c8cc58c04d14a5b97773a7e064bb8be939fbc
evan-boissonnot <commercial@boissonnot.Fr>
2020-01-18 12:06:56 +0100

    evo: collider manager serviec is done / v0.1

---
+++ v0/src/app/shared/services/collider-manager.service.ts
@@ -0,0 +1,44 @@
+import { singleton } from "tsyringe";
+import ColliderAction from '../../core/models/colliders/collider.action';
+import { BaseLevelScene } from '../../core/scenes/base-level.scene';
+import { Dictionary } from '../custom-types/dictionary';
+
+/**
+ * Service to manage calling collision action
+ * It decides wich action to launch, with valid arguments
+ */
+@singleton()
+class ColliderManagerService {
+    //#region Fields
+    private _actions: ColliderAction[];
+    private _scene: BaseLevelScene;
+    //#endregion
+
+    //#region Public methods
+    /** First step: call this method to set the array of actions */
+    init(scene: BaseLevelScene, actions: ColliderAction[]) {
+        this._actions = actions;
+        this._scene = scene;
+    }
+
+    /** Execute the valid action (if it exists) */
+    execute(transmitter: Phaser.GameObjects.Sprite, receiver: Phaser.GameObjects.Sprite) {
+        const action = this._actions.find(item => {
+            return item.transmitterKey === transmitter.name &&
+                   item.receiverKey === receiver.name;
+        });
+        
+        const actors: Dictionary<Phaser.GameObjects.Sprite> = {};
+        actors[transmitter.name] = transmitter;
+        actors[receiver.name] = receiver;
+        
+        const activeActor = actors[action.actorKey];
+        if (activeActor) {
+            const method = activeActor[action.commandName];
+            method();
+        }
+    }
+    //#endregion
+}
+
+export default ColliderManagerService;
