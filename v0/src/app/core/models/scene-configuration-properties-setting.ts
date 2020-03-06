72f0344755f0300111ad0055f07e7668912a91a0
evan-boissonnot <commercial@boissonnot.Fr>
2020-03-06 08:06:42 +0100

    evo: adding background style in json

--- v0/src/app/core/models/scene-configuration-properties-setting.ts
+++ v0/src/app/core/models/scene-configuration-properties-setting.ts
@@ -1,6 +1,7 @@
 import { PropertiesSetting } from './properties-setting';
 import { DialogModalConfiguration } from './dialog-modal/dialog-modal-configuration';
 import { StatusPlayerBoxConfiguration } from './statusBar/status-player-box-configuration';
+import { Style } from './style';
 
 /**
  * It represents properties setting of the whole scene
@@ -12,5 +13,8 @@ export class SceneConfigurationPropertiesSetting extends PropertiesSetting {
 
     /** Configuration about each box for each player : box to display stats of each player */
     public statsPlayerBox: StatusPlayerBoxConfiguration;
+
+    /** Style of the background of the scene */
+    public backgroudStyle: Style;
     //#endregion
 }
