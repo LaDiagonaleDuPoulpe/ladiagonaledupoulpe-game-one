8e544ca90b383d4cdd53a907ea62c221ad7fd18c
evan-boissonnot <commercial@boissonnot.Fr>
2020-05-08 01:17:15 +0200

    evo: adding background image + evo: adding player scene

---
+++ v0/Motor/Sources/App/Game-Scenes/002-Inside-Crash-Ship/InsideCrashShip.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public class InsideCrashShip : Node2D
+{
+	#region Fields
+	private PackedScene _player;
+	#endregion
+
+	#region Public methods
+	public override void _Ready()
+	{
+		this._player = (PackedScene) ResourceLoader.Load("res://Sources/App/Core/Models/Players/Player.tscn");
+		var instance = this._player.Instance();
+	}
+	#endregion
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
