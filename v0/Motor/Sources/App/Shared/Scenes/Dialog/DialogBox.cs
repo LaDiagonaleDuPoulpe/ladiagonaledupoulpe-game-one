e60580ddf09e8bc03ac17cf8effa23b19a4abb95
evan-boissonnot <commercial@boissonnot.Fr>
2020-05-15 08:52:54 +0200

    addons: adding debugger addon && evo: button next or close button event managed

---
+++ v0/Motor/Sources/App/Shared/Scenes/Dialog/DialogBox.cs
@@ -0,0 +1,91 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Dialog box : display content text and a texture animated sprite
+/// </summary>
+public class DialogBox : Node2D
+{
+    #region Fields
+    private List<string> _messageList = new List<string>()
+    {
+        "Hello, ca va ?",
+        "Yes et toi ?"
+    };
+    private RichTextLabel _label = null;
+    private Timer _currentTimer = null;
+    private int _currentPartOfMessage = 0;
+    #endregion
+
+    #region Public methods
+    public override void _Ready()
+    {
+        this._label = this.GetNode("Content") as RichTextLabel;
+        this._currentTimer  = this.GetNode("Timer") as Timer;
+        this.Initialize();
+    }
+
+    public void OnTimerTimeout()
+    {
+        this.CurrentVisibleCharacters++;
+    }
+
+    public void OnNextOrClosePressed()
+    {        
+        this.CurrentPartOfMessage++;
+        this._currentTimer.Stop();
+
+        bool visible = this.CurrentPartOfMessage < this.MessageList.Count;
+        this.Visible = visible;
+        if (this.Visible)
+        {
+            this.Initialize();
+            this._currentTimer.Start();
+        }
+    }
+    #endregion
+
+    #region Internal methods
+    private void Initialize()
+    {
+        this.CurrentVisibleCharacters = 0;
+        this._label.BbcodeText = this.Message;
+    }
+    #endregion
+
+    #region Properties
+    /// <summary>
+    /// Defines the number of characters to show in the label content
+    /// </summary>
+    public int CurrentVisibleCharacters { get => this._label.VisibleCharacters; set => this._label.VisibleCharacters = value; }
+
+    /// <summary>
+    /// Content message to display
+    /// </summary>
+    public string Message
+    {
+        get
+        {
+            string content = string.Empty;
+
+            if (this.CurrentPartOfMessage < this.MessageList.Count)
+            {
+                content = this.MessageList[this.CurrentPartOfMessage];
+            }
+
+            return content;
+        }
+    }
+
+    /// <summary>
+    /// Index of the part of the message in the message list
+    /// </summary>
+    public int CurrentPartOfMessage { get => this._currentPartOfMessage; private set => this._currentPartOfMessage = value; }
+
+    /// <summary>
+    /// List of message text as one full message to be displayed in X steps
+    /// </summary>
+    public List<string> MessageList { get => this._messageList; set => this._messageList = value; }
+    #endregion
+}
