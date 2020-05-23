63df4c2f09f44105be0b324d0ab4b17f51b13094
evan-boissonnot <commercial@boissonnot.Fr>
2020-05-23 09:59:39 +0200

    evo: preparing cloud generator

---
+++ v0/Motor/Sources/App/Shared/Plugins/Generators/GreyCloudGenerator.Plugin.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+/// <summary>
+/// Uses it to generate a random list of clouds in a scene
+/// </summary>
+public class GreyCloudGenerator 
+{
+    #region Consructors
+
+    #endregion
+}
