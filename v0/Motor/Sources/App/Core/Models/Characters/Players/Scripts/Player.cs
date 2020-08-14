b4184a3653095b03669aecfd631683512e3af331
evan-boissonnot <commercial@boissonnot.Fr>
2020-08-14 08:47:52 +0200

    refactor(player): Move some properties to parent
    BaseCharacter get some properties from its son

--- v0/Motor/Sources/App/Core/Models/Characters/Players/Scripts/Player.cs
+++ v0/Motor/Sources/App/Core/Models/Characters/Players/Scripts/Player.cs
@@ -1,38 +1,33 @@
 using Godot;
+using ladiagonaledupoulpe.Sources.App.Core.Models.Characters.Scripts;
 using System;
 
 /// <summary>
 /// Current player with animated sprite in the game
 /// </summary>
-public class Player : KinematicBody2D
+public class Player : BaseCharacter
 {
 	#region Fields
-	private Vector2 _screenSize;
 	private string _lastAnimation = "";
-
-	private Vector2 _velocity = Vector2.Zero;
     #endregion
 
     #region Public methods
-    public override void _Ready()
-	{
-		this.ScreenSize = this.GetViewport().Size;
-	}
-
 	public override void _PhysicsProcess(float delta)
 	{
+        base._PhysicsProcess(delta);
+
 		Vector2 vector = Vector2.Zero;
 
 		vector.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
 		vector.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
 		
-		this._velocity = Vector2.Zero;
+		this.Velocity = Vector2.Zero;
 		if (vector != Vector2.Zero) 
 		{
-			this._velocity = vector;
+			this.Velocity = vector;
 		}
 
-		this.MoveAndCollide(this._velocity.Normalized() * this.Speed);
+		this.MoveAndCollide(this.Velocity.Normalized() * this.Speed);
 	}
 
 	public override void _Process(float delta)
@@ -42,21 +37,21 @@ public class Player : KinematicBody2D
 	
 		var animatedSprite = this.GetNode<AnimatedSprite>("AnimatedSprite");
 
-		if (this._velocity.Length() <= 0)
+		if (this.Velocity.Length() <= 0)
 		{
 			prefix = "idle_";
 		}
 
-		if (this._velocity.x != 0)
+		if (this.Velocity.x != 0)
 		{
 			this._lastAnimation = "left";
 			animatedSprite.FlipV = false;
-			animatedSprite.FlipH = this._velocity.x > 0;
+			animatedSprite.FlipH = this.Velocity.x > 0;
 		}
-		else if (this._velocity.y != 0)
+		else if (this.Velocity.y != 0)
 		{
 			this._lastAnimation = "up";
-			if (this._velocity.y > 0)
+			if (this.Velocity.y > 0)
 			{
 				this._lastAnimation = "down";
 			}
@@ -69,19 +64,5 @@ public class Player : KinematicBody2D
 	#endregion
 
 	#region Properties
-	/// <summary>
-	/// Size of the game window.
-	/// </summary>
-	public Vector2 ScreenSize
-	{
-		get => this._screenSize;
-		private set => this._screenSize = value;
-	}
-
-	/// <summary>
-	/// How fast the player will move (pixels/sec)
-	/// </summary>
-	[Export]
-	public int Speed { get; set; } = 300;
 	#endregion
 }
---
+++ v0/Motor/Sources/App/Core/Models/Characters/Scripts/BaseCharacter.cs
@@ -0,0 +1,47 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ladiagonaledupoulpe.Sources.App.Core.Models.Characters.Scripts
+{
+    /// <summary>
+    /// Absract class, it represents all characters in the game : pncs anr pcs 
+    /// </summary>
+    public abstract class BaseCharacter : KinematicBody2D
+    {
+        #region Fields
+        private Vector2 _velocity = Vector2.Zero;
+        private Vector2 _screenSize;
+        #endregion
+
+        #region Public methods
+        public override void _Ready()
+        {
+            base._Ready();
+            this.ScreenSize = this.GetViewport().Size;
+        }
+        #endregion
+
+        #region Properties
+        public Vector2 Velocity { get => this._velocity; protected set => this._velocity = value; }
+
+        /// <summary>
+        /// Size of the game window.
+        /// </summary>
+        public Vector2 ScreenSize
+        {
+            get => this._screenSize;
+            private set => this._screenSize = value;
+        }
+
+        /// <summary>
+        /// How fast the player will move (pixels/sec)
+        /// </summary>
+        [Export]
+        public int Speed { get; set; } = 300;
+        #endregion
+    }
+}
