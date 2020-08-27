9c20902555d8886ff1311762cf2156d367a0b366
evan-boissonnot <commercial@boissonnot.Fr>
2020-08-27 23:16:19 +0200

    test(Test player): adding test project
    adding project for testing
    
    but it seems to fail

---
+++ v0/ladiagonaledupoulpe.Tests/PlayerUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ladiagonaledupoulpe.Sources.App.Core.Models.Characters.Players.Scripts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ladiagonaledupoulpe.Tests
+{
+    [TestClass]
+    public class PlayerUnitTest
+    {
+        #region Public methods
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Player player = new Player();
+
+            player.Hit(10);
+        }
+        #endregion
+    }
+}
