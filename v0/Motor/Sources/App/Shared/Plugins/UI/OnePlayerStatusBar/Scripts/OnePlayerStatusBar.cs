a98186cd435410e37ba1cb5d568efc89ee1fca05
evan-boissonnot <commercial@boissonnot.Fr>
2020-08-29 15:09:50 +0200

    fix(satusbar): adding one file in csproj

--- v0/Motor/Sources/App/Shared/Plugins/UI/OnePlayerStatusBar/Scripts/OnePlayerStatusBar.cs
+++ v0/Motor/Sources/App/Shared/Plugins/UI/OnePlayerStatusBar/Scripts/OnePlayerStatusBar.cs
@@ -1,21 +1,15 @@
 using Godot;
 using System;
 
+/// <summary>
+/// Status bar for all infos of one player (lifebar, synale power, ...)
+/// </summary>
 public class OnePlayerStatusBar : Node2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
+    #region Public methods
     public override void _Ready()
     {
         
     }
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    #endregion
 }
