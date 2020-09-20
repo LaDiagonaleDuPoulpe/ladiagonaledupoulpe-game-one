06f0df470c1149a2fb60b7890690f61e510bb32e
Anthony Coustes <coustes.anthony62@yahoo.fr>
2020-09-20 18:53:31 +0200

    feat(Code editor): Add button to switch frameAction

--- v0/Motor/Sources/App/Assets/Settings/Models/CompilerConfiguration.cs
+++ v0/Motor/Sources/App/Assets/Settings/Models/CompilerConfiguration.cs
@@ -6,9 +6,18 @@ using System.Threading.Tasks;
 
 namespace ladiagonaledupoulpe.Sources.App.Assets.Settings.Models
 {
+    /// <summary>
+    /// Settings Compiler Server
+    /// </summary>
     public class CompilerConfiguration
     {
+        /// <summary>
+        /// Host server api url 
+        /// </summary>
         public string HostServer { get; set; }
+        /// <summary>
+        /// Headers for configure request HTTP
+        /// </summary>
         public IList<string> Headers { get; set; }
     }
 }
