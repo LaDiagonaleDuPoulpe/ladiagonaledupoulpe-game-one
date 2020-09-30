e93bfe78ad91ce55dad185653c66c8e025ed7658
evan@boissonnot.fr <Bienvenue_2510?>
2020-09-30 11:17:18 +0200

    refactor(project + dataloader): removing some file + creating dataloader

---
+++ v0/Motor/Sources/App/Shared/Plugins/Initializers/GlobalDataInitializer.cs
@@ -0,0 +1,48 @@
+﻿using Godot;
+using ladiagonaledupoulpe.Sources.App.Shared.Interfaces.Initializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ladiagonaledupoulpe.Sources.App.Shared.Plugins.Initializers
+{
+    /// <summary>
+    /// Factory and singleton to initialize all data initializer
+    /// </summary>
+    public class GlobalDataInitializer : Node, IDataInitializer
+    {
+        #region Fields
+        #region Signals
+        /// <summary>
+        /// Beginining of the data loading
+        /// </summary>
+        [Signal]
+        public delegate void StartLoading();
+
+        /// <summary>
+        /// End of the data loading
+        /// </summary>
+        [Signal]
+        public delegate void DataLoaded();
+        #endregion
+
+        private List<IDataInitializer> _dataInitializers = new List<IDataInitializer>();
+        #endregion
+
+        #region Constructors
+        public GlobalDataInitializer()
+        {
+            this._dataInitializers.Add(new InMemoryPlayerDataInitializer());
+        }
+        #endregion
+
+        #region Public methods
+        public void Load()
+        {
+            this._dataInitializers.ForEach(item => item.Load());
+        }
+        #endregion
+    }
+}
