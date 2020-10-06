80abbe069d4a7ff105c08b8ed68715e0f7d37667
Evan BOISSONNOT <evan@boissonnot.fr>
2020-10-06 14:28:29 +0200

    fix(Scene 3): Delete hello richtext

--- v0/Motor/Sources/App/Shared/Plugins/UI/DialoxBoxManager.cs
+++ v0/Motor/Sources/App/Shared/Plugins/UI/DialoxBoxManager.cs
@@ -1,4 +1,4 @@
-﻿using Godot;
+using Godot;
 using ladiagonaledupoulpe.Sources.App.Core.Interfaces.DialogBox;
 using ladiagonaledupoulpe.Sources.App.Core.Models.DialogBox;
 using ladiagonaledupoulpe.Sources.App.Shared.Enums;
@@ -11,97 +11,97 @@ using System.Threading.Tasks;
 
 namespace ladiagonaledupoulpe.Sources.App.Shared.Services
 {
-    /// <summary>
-    /// Manager of the dialogbox of one scene.
-    /// Allows you to command dialog box of the scene : some messages will be auto launched, some others will be load by signal or key.
-    /// </summary>
-    public class DialoxBoxManager : Node, IDialogBoxManager
-    {
-        #region Fields
-        #region Signals
-        /// <summary>
-        /// Occurs when all messages of one exchange in dialogbox are done
-        /// </summary>
-        [Signal]
-        public delegate void EndOfOneExchange();
-        #endregion
+	/// <summary>
+	/// Manager of the dialogbox of one scene.
+	/// Allows you to command dialog box of the scene : some messages will be auto launched, some others will be load by signal or key.
+	/// </summary>
+	public class DialoxBoxManager : Node, IDialogBoxManager
+	{
+		#region Fields
+		#region Signals
+		/// <summary>
+		/// Occurs when all messages of one exchange in dialogbox are done
+		/// </summary>
+		[Signal]
+		public delegate void EndOfOneExchange();
+		#endregion
 
-        private Timer _timer = null;
-        private List<DialogBoxExchange> _exchanges = null;
-        private DialogBoxExchange _currentExchange = null;
-        private DialogBox _dialogBox = null;
-        #endregion
+		private Timer _timer = null;
+		private List<DialogBoxExchange> _exchanges = null;
+		private DialogBoxExchange _currentExchange = null;
+		private DialogBox _dialogBox = null;
+		#endregion
 
-        #region Constructors
-        private DialoxBoxManager() { }
-        #endregion
+		#region Constructors
+		private DialoxBoxManager() { }
+		#endregion
 
-        #region Public methods
-        public override void _Ready()
-        {
-            base._Ready();
+		#region Public methods
+		public override void _Ready()
+		{
+			base._Ready();
 
-            this.DialogBox = this.GetNode<DialogBox>("/root/DialogBox");
-            this.DialogBox.Connect(DialogBoxActionType.EndOfAllMessages.ToString(), this, nameof(StopDisplayMessagesOfOneExchange));
+			this.DialogBox = this.GetNode<DialogBox>("/root/DialogBox");
+			this.DialogBox.Connect(DialogBoxActionType.EndOfAllMessages.ToString(), this, nameof(StopDisplayMessagesOfOneExchange));
 
-            this.CreateTimer();
-        }
+			this.CreateTimer();
+		}
 
-        /// <summary>
-        /// Preloads data about the dialog box
-        /// </summary>
-        public void Preload(List<DialogBoxExchange> contents)
-        {
-            this._exchanges = contents;
-        }
+		/// <summary>
+		/// Preloads data about the dialog box
+		/// </summary>
+		public void Preload(List<DialogBoxExchange> contents)
+		{
+			this._exchanges = contents;
+		}
 
-        /// <summary>
-        /// Starts a dialog box exchange
-        /// </summary>
-        /// <param name="key">Key of exchange</param>
-        public void Start(string key)
-        {
-            this._currentExchange = this._exchanges.First(item => item.Key == key);
-            this._timer.Start(this._currentExchange.TimeBeforeStart / 1000);
-        }
-        #endregion
+		/// <summary>
+		/// Starts a dialog box exchange
+		/// </summary>
+		/// <param name="key">Key of exchange</param>
+		public void Start(string key)
+		{
+			this._currentExchange = this._exchanges.First(item => item.Key == key);
+			this._timer.Start(this._currentExchange.TimeBeforeStart / 1000);
+		}
+		#endregion
 
-        #region Internal methods
-        private void CreateTimer()
-        {
-            this._timer = new Timer();
-            this._timer.Autostart = false;
-            this.AddChild(this._timer);
+		#region Internal methods
+		private void CreateTimer()
+		{
+			this._timer = new Timer();
+			this._timer.Autostart = false;
+			this.AddChild(this._timer);
 
-            this._timer.Connect("timeout", this, nameof(ActiveMethodAfterTimeOut));
-        }
+			this._timer.Connect("timeout", this, nameof(ActiveMethodAfterTimeOut));
+		}
 
-        private void ActiveMethodAfterTimeOut()
-        {
-            this._timer.Stop();
-            if (this._currentExchange != null)
-            {
-                this.DialogBox.Start(this._currentExchange.Messages);
-                this._currentExchange = null;
-            }
-        }
+		private void ActiveMethodAfterTimeOut()
+		{
+			this._timer.Stop();
+			if (this._currentExchange != null)
+			{
+				this.DialogBox.Start(this._currentExchange.Messages);
+				this._currentExchange = null;
+			}
+		}
 
-        private void StopDisplayMessagesOfOneExchange()
-        {
-            this.EmitSignal(DialogBoxActionType.EndOfOneExchange.ToString());
-        }
+		private void StopDisplayMessagesOfOneExchange()
+		{
+			this.EmitSignal(DialogBoxActionType.EndOfOneExchange.ToString());
+		}
 
-        private void ShowDialog()
-        {
-            GD.Print("ShowDialog event, 13:07/2020, seems to not run ... for now, don't now why ...");
-        }
-        #endregion
+		private void ShowDialog()
+		{
+			GD.Print("ShowDialog event, 13:07/2020, seems to not run ... for now, don't now why ...");
+		}
+		#endregion
 
-        #region Properties
-        /// <summary>
-        /// Dialog box scene to display exchange items
-        /// </summary>
-        public DialogBox DialogBox { get => this._dialogBox; private set => this._dialogBox = value; }
-        #endregion
-    }
+		#region Properties
+		/// <summary>
+		/// Dialog box scene to display exchange items
+		/// </summary>
+		public DialogBox DialogBox { get => this._dialogBox; private set => this._dialogBox = value; }
+		#endregion
+	}
 }
