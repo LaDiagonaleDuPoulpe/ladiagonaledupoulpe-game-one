de7511d1b6b2bc6735d12a02eeb80a3545280ea1
Evan BOISSONNOT <evan@boissonnot.fr>
2020-10-06 17:14:48 +0200

    refactor(signals): using autoloaderaccessor
    Use the proxy class

--- v0/Motor/Sources/App/Shared/Signals/Signals.cs
+++ v0/Motor/Sources/App/Shared/Signals/Signals.cs
@@ -2,6 +2,7 @@ using Godot;
 using ladiagonaledupoulpe.Sources.App.Core.Models.Characters.Players.Scripts;
 using ladiagonaledupoulpe.Sources.App.Core.Models.Characters.Scripts;
 using ladiagonaledupoulpe.Sources.App.Shared.Enums;
+using ladiagonaledupoulpe.Sources.App.Shared.Plugins;
 using System;
 
 /// <summary>
@@ -13,13 +14,16 @@ public class Signals : Node
     #region Fields
     private Player _currentPlayer = null;
     private OnePlayerStatusBar _statusBar = null;
+    private AutoLoaderAccessor _autoLoaderAccessor = null;
     #endregion
 
     #region Public methods
     public override void _Ready()
     {
-        this._currentPlayer = this.GetNode<Player>("/root/CurrentPlayer");
-        this._statusBar = this.GetNode<OnePlayerStatusBar>("/root/OnePlayerStatusBar");
+        this._autoLoaderAccessor = this.GetNode<AutoLoaderAccessor>("/root/AutoLoaderAccessor");
+
+        this._currentPlayer = this._autoLoaderAccessor.CurrentPlayer;
+        this._statusBar = this._autoLoaderAccessor.StatusBar;
 
         this._currentPlayer.Connect(CharacterLifeSignal.HealthChanged.ToString(), this._statusBar, "LifeChanged");
     }
