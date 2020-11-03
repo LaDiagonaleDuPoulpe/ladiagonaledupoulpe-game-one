9aabf5d5d2cf9e0a5b5a2f601544eb400d950a25
Evan BOISSONNOT <evan@boissonnot.fr>
2020-11-04 00:02:26 +0100

    feat(preloaddataloader): prepare the plugin
    add the plugin in auto loader part of the project

---
+++ v0/Motor/Sources/App/Shared/Plugins/Preloaders/MainDataPreloader.cs
@@ -0,0 +1,16 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ladiagonaledupoulpe.Sources.App.Shared.Plugins.Preloaders
+{
+    /// <summary>
+    /// Uses it to preload the first necessary data before load the root scene
+    /// </summary>
+    public class MainDataPreloader : Node
+    {
+    }
+}
